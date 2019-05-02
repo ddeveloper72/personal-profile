8e29b1dc78b48703015eb50c91e4f859d0c45e71
ddeveloper72 <duncan.niamh@gmail.com>
2019-05-02 13:41:51 +0100

    Updated 404 view and template block tags.

--- run.py
+++ run.py
@@ -56,9 +56,9 @@ def about_education(education_name):
 @app.errorhandler(404)
 def page_not_found(e):
     """ 
-    note that we set the 404 status explicitly 
+    Inbuilt function which takes error as parameter 
     """
-    return render_template('404.html'), 404
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'),
